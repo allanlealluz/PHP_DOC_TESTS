1270e38a9f4c895fb359360c7579bb9c6a372311
allanlealluz <allanlealluz@gmail.com>
2024-01-21 20:17:13 -0300

    other stuff

--- tests3.php
+++ tests3.php
@@ -4,3 +4,21 @@ define('PADARIA', 'valor');
 define('variable', $variable);
 echo PADARIA;
 echo variable;
+$x = 1;
+$y = 3 * (abs(2 * $x) + 4);
+$num = 1*6+5-8/2+15;
+echo "<br>$num";
+echo "<br>$y";
+function countBy($x, $n) {
+    $val = $x;$count = 1;
+  $z = [];
+    while($count <= $n){
+      $z[]= $count;
+      $count += $x;
+    }
+    return $z;
+}
+$t = countBy(2,5);
+foreach($t as $v){
+    echo $v;
+}
